c8dfca0f3c4b67f4a2096978dfb76f5e271fc8b8
OniqLoshiq <doncho.vasilev@gmail.com>
2018-03-29 00:53:54 +0300

    C# Fundamentals - OOP Advanced

--- 06- 180320-C# OOP Advanced/180327-OOPA-7 Ex 3 - Iter&Comp/180327-OOPA-7 Ex 3/02-Collection/ListyIterator.cs
+++ 06- 180320-C# OOP Advanced/180327-OOPA-7 Ex 3 - Iter&Comp/180327-OOPA-7 Ex 3/02-Collection/ListyIterator.cs
@@ -4,38 +4,35 @@ using System.Collections.Generic;
 
 public class ListyIterator<T> : IEnumerable<T>
 {
-    private readonly List<T> List;
+    private readonly IReadOnlyList<T> Collection;
     private int index = 0;
 
-    public ListyIterator()
-    {
-
-    }
+    public ListyIterator() { }
 
     public ListyIterator(T[] list) : this()
     {
-        this.List = new List<T>(list);
+        this.Collection = new List<T>(list);
     }
 
     public bool Move()
     {
-        var result = ++this.index < this.List.Count;
+        var result = ++this.index < this.Collection.Count;
 
         if(!result)
         {
-            index--;
+            this.index--;
         }
 
         return result;
     }
 
-    public bool HasNext() => this.index >= this.List.Count - 1 ? false : true;
+    public bool HasNext() => this.index >= this.Collection.Count - 1 ? false : true;
 
     public void Print()
     {
         CheckIfEmpty();
 
-        Console.WriteLine(this.List[index].ToString());
+        Console.WriteLine(this.Collection[index]);
     }
         
     public void PrintAll()
@@ -44,7 +41,7 @@ public class ListyIterator<T> : IEnumerable<T>
 
         string result = String.Empty;
 
-        foreach (T item in this.List)
+        foreach (T item in this.Collection)
         {
             result += item + " ";
         }
@@ -54,15 +51,15 @@ public class ListyIterator<T> : IEnumerable<T>
 
     private void CheckIfEmpty()
     {
-        if (this.List.Count == 0)
+        if (this.Collection.Count == 0)
             throw new InvalidOperationException("Invalid Operation!");
     }
 
     public IEnumerator<T> GetEnumerator()
     {
-        for (int i = 0; i < this.List.Count; i++)
+        for (int i = 0; i < this.Collection.Count; i++)
         {
-            yield return this.List[i];
+            yield return this.Collection[i];
         }
     }
 
